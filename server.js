dc8bb3e4642c6e5090956046d9c725e3c463ed18
Ndvschen8 <113867126+Ndvschen8@users.noreply.github.com>
2026-05-18 17:07:22 -0400

    Fix portal: use req.lorgnerUser.id not .sub

--- server.js
+++ server.js
@@ -432,7 +432,7 @@ app.post('/create-checkout-session', async (req, res) => {
 app.post('/create-portal-session', requireAuth, async (req, res) => {
   try {
     const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
-    const userId = req.lorgnerUser.sub;
+    const userId = req.lorgnerUser.id;
 
     const memberRes = await fetch(
       `${CONFIG.SUPABASE_URL}/rest/v1/members?id=eq.${userId}&select=stripe_customer_id`,
